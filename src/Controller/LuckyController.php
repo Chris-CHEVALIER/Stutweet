92e0249833dff8ff57930987757177ed7ac5447e
Chevalier <chevalierchris34@gmail.com>
2022-08-24 10:53:41 +0200

    creating first controllers & routes

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("lucky")
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            "<html>
+                <h1>Votre nombre fétiche : $number</h1>
+            </html>"
+        );
+    }
+
+    #[Route("random")]
+    public function random(): Response
+    {
+        return $this->render("/lucky/number.html.twig", [
+            "number" => random_int(0, 100)
+        ]);
+    }
+}
